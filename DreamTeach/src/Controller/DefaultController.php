4a9eb3dea3e5c41ff316069e4d4684554ba3f8ee
Arnaud Sain-Rakoto <arnaud.sainrak@gmail.com>
2019-01-30 14:08:18 +0100

    first commit arnaud

---
+++ DreamTeach/src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function defaultAction()
+    {
+        return $this->render('base.html.twig');
+    }
+}

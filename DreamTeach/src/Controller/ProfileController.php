20f74efb1dc82ef0a3142cf12c57528071f9dedb
Alexandre <chronoos89@gmail.com>
2019-02-04 18:58:27 +0100

    Fix

--- DreamTeach/src/Controller/ProfileController.php
+++ DreamTeach/src/Controller/ProfileController.php
@@ -24,25 +24,12 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile/{idStudent}", name="profile")
      */
-    public function getInfoStudent($idStudent) {
+    public function getInfoStudent($idStudent)
+    {
         $reqUser = $this->getDoctrine()->getRepository(Student::Class);
         $user = $reqUser->find($idStudent);
-        $idUser = $user->getId();
-        if($idUser == $this->getUser()->getId()) {
-            return $this->render("myProfile.html.twig", ["user" => $user]);
-        } else {
-            return $this->render("viewProfile.html.twig", ["user" => $user]);
-        }
-    }
-    /**
-     * @Route("/myProfile/{idStudent}", name="profile")
-     */
-    public function viewMyProfile($idStudent) {
-        $reqUser = $this->getDoctrine()->getRepository(Student::Class);
-        $user = $reqUser->find($idStudent);
-        $idUser = $user->getId();
-        return $this->render("myProfile.html.twig", ["user" => $user]);
 
+        return $this->render("viewProfile.html.twig", ["user" => $user]);
     }
 
     /**

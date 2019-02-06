220d222724aa65dbd1c112c2ecc9eaa864689d1a
claurence <camille.laurence196@gmail.com>
2019-02-06 13:20:50 +0100

    Modif agenda

--- DreamTeach/src/Controller/AgendaController.php
+++ DreamTeach/src/Controller/AgendaController.php
@@ -8,8 +8,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Entity\Training;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class StudentController
+ * @package App\Controller
+ * @Route("/dashboard")
+ * @IsGranted("ROLE_USER")
+ */
 class AgendaController
 {
+    /**
+     * @Route("/accueil/agenda", name="agenda")
+     */
 
+    public function homeAgendaAction()
+    {
+        return $this->render("myAgenda.html.twig");
+    }
 }

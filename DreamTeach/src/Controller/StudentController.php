1783b913c6eb989872f0dee476718461a519683b
claurence <camille.laurence196@gmail.com>
2019-02-11 14:06:29 +0100

    Verif séance antérieur dans la liste

--- DreamTeach/src/Controller/StudentController.php
+++ DreamTeach/src/Controller/StudentController.php
@@ -31,7 +31,17 @@ class StudentController extends AbstractController
 
     public function homeStudentAction()
     {
-        $session = $this->getDoctrine()->getRepository(Session::class)->findAll();
+        $tpm = array();
+        $session = $this->getDoctrine()->getRepository(Session::class)->findall();
+
+        foreach($session as $key => $value)
+        {
+            $now = new \DateTime();
+            if($value->getDate() > $now){
+            array_push($tpm, $value);
+            }
+        }
+
         $student = $this->getUser();
         /** @var Session $listeSession */
         $listeSession = $student->getSessionid();
@@ -51,7 +61,8 @@ class StudentController extends AbstractController
             // On ajoute les ID des sessions
             array_push($listeSessionEtudiant, $ss);
         }
-        return $this->render("dashboard.html.twig", ['session' => $session, 'sessionUser' => $listeSessionEtudiant]);
+
+        return $this->render("dashboard.html.twig", ['session' => $tpm, 'sessionUser' => $listeSessionEtudiant]);
     }
 
     /**

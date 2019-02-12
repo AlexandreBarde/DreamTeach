6d495b4f9d862d7d8ca94ac5e14a2bfe43ad12d9
Hugo <Hugo.boehm98@gmail.com>
2019-02-12 14:28:38 +0100

    Ajout front remplir ses compétences

--- DreamTeach/src/Entity/Subject.php
+++ DreamTeach/src/Entity/Subject.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Subject
  *
  * @ORM\Table(name="subject")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\SubjectRepository")
  */
 class Subject
 {

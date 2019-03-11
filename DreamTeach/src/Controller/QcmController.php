b13fcaeeb3ad1ab2eca0e74b4aa9380eb994ac70
MEKKI ADEL <adel.mekki1998@hotmail.com>
2019-03-11 15:52:54 +0100

    wip - create qcm

--- DreamTeach/src/Controller/QcmController.php
+++ DreamTeach/src/Controller/QcmController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 
 use App\Entity\Qcm;
+use App\Form\CreateQcmType;
 use App\Form\EditQcm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class MessageController
@@ -50,6 +52,30 @@ class QcmController extends AbstractController
         $form = $this->createForm(EditQcm::class, $qcm);
 
         return $this->render('qcm.edit.html.twig', ["editQcm" => $form->createView()]);
+
+    }
+
+    /**
+     * Permet de créer un QCM
+     * @Route("createQcm", name="createQcm")
+     */
+    public function createQcm(Request $request)
+    {
+        $qcm = new Qcm();
+
+        $formQcm = $this->createForm(CreateQcmType::class, $qcm);
+        $formQcm->handleRequest($request);
+
+        if($formQcm->isSubmitted() && $formQcm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $qcm->setAuthorId($this->getUser());
+            $em->persist($qcm);
+            $em->flush();
+            return $this->redirectToRoute('showQcms');
+
+        }
+
+        return $this->render('qcm.create.html.twig', ["formCreateQcm" => $formQcm->createView()]);
 
     }
 

484d71a28ab4906f262140c5b341d3c6d7bac2cd
Alexandre <chronoos89@gmail.com>
2019-03-20 17:40:55 +0100

    wip - front

--- DreamTeach/src/Controller/HangmanController.php
+++ DreamTeach/src/Controller/HangmanController.php
@@ -8,8 +8,24 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HangmanController
+/**
+ * Class MessageController
+ * @package App\Controller
+ * @IsGranted("ROLE_USER")
+ */
+class HangmanController extends AbstractController
 {
+
+    /**
+     * @Route("hangman", name="PlayHangman")
+     */
+    public function showHangmanGame()
+    {
+        return $this->render("hangman.html.twig");
+    }
 
 }

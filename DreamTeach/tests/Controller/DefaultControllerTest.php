661efffb811266d8e2c578a00cceaaefb010b264
ChloeLewandowski <chloelewandowski98@gmail.com>
2019-03-22 12:06:45 +0100

    :white_check_mark: first test uploaded

---
+++ DreamTeach/tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chloé Lewandowski
+ * Date: 22/03/2019
+ * Time: 11:07
+ */
+
+namespace App\Tests;
+
+use App\Controller\DefaultController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+
+    public function testShowRegister(){
+
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}

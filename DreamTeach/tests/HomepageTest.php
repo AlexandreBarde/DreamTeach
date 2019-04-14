1e550553a158c083903b4540aa9348aa71f062f6
MEKKI ADEL <adel.mekki1998@hotmail.com>
2019-04-14 10:08:19 +0200

    Test: Mot de passe oublié Refactoring de quelques tests

--- DreamTeach/tests/HomepageTest.php
+++ DreamTeach/tests/HomepageTest.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class HomepageTest extends WebTestCase
 {
 
-    public function testCodeReturn()
+    public function testSimpleCodeReturn()
     {
         $client = static::createClient();
 
@@ -21,24 +21,22 @@ class HomepageTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
-    public function testPageFailedConnexion()
+    public function testInvalidConnexion()
     {
+        // Requete sur "/" et on vérifie qu'il n'y a pas de message d'erreur avant d'avoir cliqué sur "connexion"
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
         $this->assertLessThan(
             1,
             $crawler->filter('html:contains("L\'adresse email n\'a pas été trouvé !")')->count()
         );
 
+        // Click sur le bouton "Connexion"
         $form = $crawler->selectButton('Connexion')->form();
         $client->submit($form);
 
-        // Récupération de la page après envoi du formulaire vide
+        // Récupération de la page après envoi du formulaire vide et vérification de la présence du message d'erreur
         $crawler = $client->request('GET', '/');
-
-        // Check that after clicking "Connexion" button with wrong informations in form, the page will contain an error message
         $this->assertGreaterThan(
             0,
             $crawler->filter('html:contains("L\'adresse email n\'a pas été trouvé !")')->count()
@@ -50,27 +48,22 @@ class HomepageTest extends WebTestCase
     {
         $client = static::createClient();
 
-        // Vérifie que la page "/dashboard" n'est pas accessible et renvoie bien un code 302 (redirection) lorsqu'on est pas connectés
+        // Vérifie que la page "/dashboard" n'est pas accessible et renvoie bien un code 302 (redirection) lorsqu'on est pas connecté
         $client->request('GET', '/dashboard');
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
 
-        // Remplissage du formulaire avec identifiants valides sur la page de login
-        $crawler = $client->request('GET', '/');
-        $form = $crawler->selectButton('Connexion')->form();
+        // Connexion avec les identifiants passés en paramètres de la fonction (identifiants valides et à modifier selon le pc
+        $crawler = $this->connexionLoginPage($client, $userEmail, $userPassword);
 
-        $form['emailaddress'] = $userEmail;
-        $form['password'] =  $userPassword;
-        $client->submit($form);
-        // On vérifie que la page ne contient pas le message de bienvenue (connexion invalide)
+        // On vérifie que sur la route "/" (requête effectuée par la fonction connexionLoginPage) on a pas le message de bienvenue
         $this->assertLessThan(
             1,
             $crawler->filter('html:contains("Bienvenue sur votre espace personnel")')->count()
         );
 
-        // Vérifie que la page "/dashboard" est accessible et renvoie bien un code 200 lorsqu'on est connecté
+        // Vérifie que la page "/dashboard" est accessible et renvoie bien un code 200 lorsqu'on est connecté ainsi que le message de bienvenue
         $crawler = $client->request('GET', '/dashboard');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // On vérifie que la page contient bien le message de bienvenue (connexion valide)
         $this->assertGreaterThan(
             0,
             $crawler->filter('html:contains("Bienvenue sur votre espace personnel")')->count()
@@ -81,37 +74,30 @@ class HomepageTest extends WebTestCase
     {
         $client = static::createClient();
 
-        // Vérifie que la page "/dashboard" n'est pas accessible et renvoie bien un code 302 (redirection) lorsqu'on est pas connectés
+        // Vérifie que la page "/" est accessible et renvoi le bon code de retour
         $crawler = $client->request('GET', '/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
+        // Récupération et click sur le lien d'inscription
         $linkRegister = $crawler
             ->filter('a:contains("S\'inscrire")') // find all links with the text "S'inscrire"
-            ->eq(0) // select the second link in the list
+            ->eq(0) // selectionne le 1er lien trouvé
             ->link()
         ;
+
+        // Vérifie que la redirection n'a pas encore été effectuée
+        $this->assertNotEquals('http://localhost/register', $crawler->getUri());
         $crawler = $client->click($linkRegister);
 
+        // Vérifie que la redirection a bien été effectuée
         $this->assertEquals('http://localhost/register', $crawler->getUri());
-
+        return $crawler;
     }
 
     public function testInscriptionValide()
     {
         $client = static::createClient();
 
-        // Vérifie que la page "/dashboard" n'est pas accessible et renvoie bien un code 302 (redirection) lorsqu'on est pas connectés
-        $crawler = $client->request('GET', '/');
-
-        // Click bouton s'inscrire
-        $linkRegister = $crawler
-            ->filter('a:contains("S\'inscrire")') // find all links with the text "S'inscrire"
-            ->eq(0) // select the second link in the list
-            ->link()
-        ;
-        $crawler = $client->click($linkRegister);
-        $this->assertEquals('http://localhost/register', $crawler->getUri());
-
+        $crawler = $this->testClickInscriptionButton();
         $userFirstName = 'Jean';
         $userLastName = 'Donacien';
         $userPassword = 'JeanDonacien';
@@ -125,8 +111,38 @@ class HomepageTest extends WebTestCase
         $form['register[password]'] = $userPassword;
         $client->submit($form);
 
-
         //Connexion avec les identifiants d'inscriptions pour vérifier que l'inscription a bien été faite
         $this->testValidConnexion($userEmail, $userPassword);
+    }
+
+    function connexionLoginPage ($client, $username, $password)
+    {
+        // Remplissage du formulaire avec identifiants valides sur la page de login
+        $crawler = $client->request('GET', '/');
+        $form = $crawler->selectButton('Connexion')->form();
+
+        $form['emailaddress'] = $username;
+        $form['password'] =  $password;
+        return $client->submit($form);
+        // On vérifie que la page ne contient pas le message de bienvenue (connexion invalide)
+    }
+
+    function testClickForgotPassword ()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $linkForgotPassword = $crawler
+            ->filter('a:contains("Mot de passe oublié ?")') // find all links with the text "S'inscrire"
+            ->eq(0) // sélectionne le 1er lien trouvé
+            ->link()
+        ;
+        // Vérifie que la redirection n'a pas encore été effectuée
+        $this->assertNotEquals('http://localhost/forgotPassword', $crawler->getUri());
+        $crawler = $client->click($linkForgotPassword);
+
+        // Vérifie que la redirection a bien été effectuée
+        $this->assertEquals('http://localhost/forgotPassword', $crawler->getUri());
     }
 }

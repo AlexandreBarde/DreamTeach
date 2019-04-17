795691e91f253b62293356bd3bbd386b2e2b23d2
Fantemis <adel.mekki1998@hotmail.com>
2019-04-17 14:48:26 +0200

    :white_check_mark: rename login de connexion

--- DreamTeach/tests/DashboardTest.php
+++ DreamTeach/tests/DashboardTest.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DashboardTest extends WebTestCase
 {
-    public function testValidConnexion($userEmail = 'adel.mekki1998@hotmail.com', $userPassword = 'adelmekki')
+    public function testValidConnexion($userEmail = 'test@mail.com', $userPassword = 'test12345678')
     {
         $client = static::createClient();
 
